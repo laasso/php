1eee674169147e7b3a3721975b21223a8e2689bd
laasso <nrubialesguerrero@gmail.com>
2024-10-22 20:07:00 +0200

    suma con parametros

---
+++ 221024/suma.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Mi primer PHP</title>
+</head>
+<body>
+
+<?php
+
+$numero = $_['num1'];
+
+$numero2 = $_['num2'];
+
+$resultado =  $numero + $numero2;
+
+echo "Hola mundo de asix<br/>";
+
+echo "La suma es:", $resultado;
+
+?>
+
+</body>
+</html>
+

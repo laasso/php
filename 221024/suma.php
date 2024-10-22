5474c6f8632c5718bb0485683d90ae7dd18cbc6e
laasso <nrubialesguerrero@gmail.com>
2024-10-22 20:24:09 +0200

    suma amb formulari

--- 221024/suma.php
+++ 221024/suma.php
@@ -4,6 +4,11 @@
 </head>
 <body>
 
+<form action="suma.php" method="GET">
+<input type="text" name="num1"/>
+<input type="text" name="num2"/>
+<input type="submit" name="submit" value ="sumar/>
+</form
 <?php
 
 $numero = $_GET['num1'];

1d4ac7d7ebe8078e67ec1a451f59584f03e417f5
laasso <nrubialesguerrero@gmail.com>
2024-10-22 19:44:38 +0200

    fail

--- script.php
+++ script.php
@@ -5,6 +5,6 @@ $numero2 = 24;
 
 $resultado =  $numero + $numero2;
 
-echo "Hola mundo de asix el resultado de la suma es: "$resultado;
+echo "Hola mundo de asix el resultado de la suma es: ",$resultado;
 ?>
 

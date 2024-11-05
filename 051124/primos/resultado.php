b00676fb897ef03673d01984adf5a811fc3af6a8
laasso <nrubialesguerrero@gmail.com>
2024-11-05 20:20:13 +0100

    divisores

--- 051124/primos/resultado.php
+++ 051124/primos/resultado.php
@@ -2,21 +2,24 @@
 
 $numero = $_GET['numero'];
 $i = 3;
-$primo =$numero%2!=0 || $numero==2;
+$primo = $numero % 2 != 0 || $numero == 2;
 
-while ($primo && $i<$numero) {
-	$primo=($numero%$i) !=0;
-	$i=$i+2;
+while ($primo && $i < $numero) {
+    $primo = ($numero % $i) != 0;
+    $i = $i + 2;
 }
 
 if ($primo) {
-	echo "EL numero $numero es primo";
-} else { 
-	echo "No es primo:";
+    echo "El número $numero es primo";
+} else {
+    echo "No es primo. Sus divisores son: ";
+    
+    for ($j = 1; $j <= $numero; $j++) {
+        if ($numero % $j == 0) {
+            echo $j . " ";
+        }
+    }
 }
 
-
-
-
-
 ?>
+

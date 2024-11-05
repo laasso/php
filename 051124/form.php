70d159ef38454b9824200429d6e4e5d6f1b62fcb
laasso <nrubialesguerrero@gmail.com>
2024-11-05 19:09:14 +0100

    salto de liena

--- 051124/form.php
+++ 051124/form.php
@@ -4,14 +4,21 @@
 </head>
 <body>
 Introdueix les seguents dades: <br/>
-<form action="dades.php" method="GET">
-Nom: <input type="text" name="name"/>
-Adreca: <input type="text" name="adreca"/>
-Mail: <input type="mail" name="mail"/>
-Telefon: <input type="telefon" name="telefon"/>
-Data Naixement:<input type="text" name="datanaix"/>
+<form action="dades.php" method="GET"><br/>
+
+Nom: <input type="text" name="name"/><br/>
+
+Adreca: <input type="text" name="adreca"/><br/>
+
+Mail: <input type="mail" name="mail"/><br/>
+
+Telefon: <input type="telefon" name="telefon"/><br/>
+
+Data Naixement:<input type="text" name="datanaix"/><br/>
+
 <input type="radio" name="sexe" value="Hi"/>Home<br/>
-<input type="radio" name="sexe" value="D"/>Dona<br/>
+<input type="radio" name="sexe" value="D"/>Dona<br/><br/>
+
 <select name="treballar">
 <option value="Si">Si</option>
 <option value="No">Si</option>

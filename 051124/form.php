bb37f33f0728373bc91d3773c1d6adfeb5007f13
laasso <nrubialesguerrero@gmail.com>
2024-11-05 19:05:55 +0100

    form

---
+++ 051124/form.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Formulario PHP</title>
+</head>
+<body>
+Introdueix les seguents dades: <br/>
+<form action="dades.php" method="GET">
+Nom: <input type="text" name="name"/>
+Adreca: <input type="text" name="adreca"/>
+Mail: <input type="mail" name="mail"/>
+Telefon: <input type="telefon" name="telefon"/>
+Data Naixement:<input type="text" name="datanaix"/>
+<input type="radio" name="sexe" value="Hi"/>Home<br/>
+<input type="radio" name="sexe" value="D"/>Dona<br/>
+<select name="treballar">
+<option value="Si">Si</option>
+<option value="No">Si</option>
+<select/>
+<input type="submit" name="submit" value ="sumar"/>
+</form>
+
+
+</body>
+</html>
+

10e6fe2f3aedd0eecff1ae0b7401c0c33d56b9e4
laasso <nrubialesguerrero@gmail.com>
2024-11-05 19:46:18 +0100

    errores

--- 051124/dades.php
+++ 051124/dades.php
@@ -13,24 +13,20 @@ echo "Nombre: " . $nombre . "<br/>";
 echo "Adreça: " . $adreca . "<br/>";
 echo "Mail: " . $mail . "<br/>";
 echo "Teléfono: " . $telefono . "<br/>";
-echo "Sexo: " . $sexe . "<br/>";
-echo "Traballar: " . $treballar;
 
-<br/>
-
+// Determina el sexo
 if ($sexe == "H") {
-	echo "Tu sexo es Masculino";
- } else {
-	echo "Tu sexo es Femenino:";
-}
-
-
-<br/>
-
-if ($treballar == "Si") {
-	echo "Estas currando";
+    echo "Tu sexo es Masculino<br/>";
 } else {
-	echo "No estas currando";
+    echo "Tu sexo es Femenino<br/>";
 }
+
+// Verifica si está trabajando
+if ($treballar == "Si") {
+    echo "Estás currando<br/>";
+} else {
+    echo "No estás currando<br/>";
+}
+
 ?>
 

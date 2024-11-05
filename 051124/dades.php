a1783db92db7ce74e118b4bc9d1d0e10f1218d0f
laasso <nrubialesguerrero@gmail.com>
2024-11-05 19:48:04 +0100

    delette comentarios

--- 051124/dades.php
+++ 051124/dades.php
@@ -14,14 +14,12 @@ echo "Adreça: " . $adreca . "<br/>";
 echo "Mail: " . $mail . "<br/>";
 echo "Teléfono: " . $telefono . "<br/>";
 
-// Determina el sexo
 if ($sexe == "H") {
     echo "Tu sexo es Masculino<br/>";
 } else {
     echo "Tu sexo es Femenino<br/>";
 }
 
-// Verifica si está trabajando
 if ($treballar == "Si") {
     echo "Estás currando<br/>";
 } else {

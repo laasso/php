1ecb349bbc56b3962d748d9e9795cb481d322e03
laasso <nrubialesguerrero@gmail.com>
2024-12-17 19:38:47 +0100

    feat: agregar un salto de línea adicional en la visualización de montañas

--- 101224/index.php
+++ 101224/index.php
@@ -26,11 +26,10 @@ if (!isset($_SESSION['muntanyes'])) {
                     <strong>Fecha de ascenso:</strong> <?= htmlspecialchars($muntanya['data_ascens']) ?><br>
                     <strong>Dificultad:</strong> <?= htmlspecialchars($muntanya['dificultat']) ?><br>
                     <strong>Actividades:</strong> <?= htmlspecialchars($muntanya['activitats']) ?><br>
-                    
+                    <br>
                     <?php if (!empty($muntanya['foto_muntanya'])): ?>
                         <img src="<?= htmlspecialchars($muntanya['foto_muntanya']) ?>" alt="Foto de <?= htmlspecialchars($muntanya['nom_muntanya']) ?>">
                     <?php endif; ?>
-                    <br>
                     <br>
                     <a href="delete.php?id=<?= urlencode($id) ?>">Eliminar</a>
                     <a href="edit_muntanyes.php?id=<?= urlencode($id) ?>">Editar</a>
